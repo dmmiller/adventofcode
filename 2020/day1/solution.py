366cc27a1eff205ed804a1aa8e53bc5de609672e
Dave Miller <dmiller@lyft.com>
2020-12-04 00:04:57 -0800

    Update to use set rather than hash map

--- 2020/day1/solution.py
+++ 2020/day1/solution.py
@@ -1,15 +1,14 @@
 file = open('input.txt')
-numbers = map(int, file.readlines())
-number_map = dict(map(lambda x : (x, 1), numbers))
-for num in number_map:
-    if 2020 - num in number_map:
+numbers = set(map(int, file.readlines()))
+for num in numbers:
+    if 2020 - num in numbers:
         print("Found the number pair ", num, 2020 - num, " with product : ", num * (2020 - num))
         break
 
-for num in number_map:
-    for num2 in number_map:
+for num in numbers:
+    for num2 in numbers:
         if num != num2:
-            if 2020 - (num + num2) in number_map:
+            if 2020 - (num + num2) in numbers:
                 print("Found the triple ", num, num2, 2020 - (num + num2), "with product : ", num * num2 * (2020 - (num + num2)))
                 break
     else:
