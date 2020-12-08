bcff2fe85f7bdfdd7bc42b52d554b72e4d491648
Dave Miller <dmiller@lyft.com>
2020-12-07 23:11:01 -0800

    Add type hints

--- 2020/day8/solution.py
+++ 2020/day8/solution.py
@@ -1,24 +1,29 @@
 import re
+import typing
+from collections.abc import Iterable, Callable
+
+#Type Information
+Instruction = tuple[str, int]
 
 instruction_re = re.compile(r"(?P<instruction>\w{3}) (?P<value>[+-]\d+)")
 
 class Machine:
 
-    def _acc(self, value):
+    def _acc(self, value : int) -> int:
         self.acc += value
         return 1
 
-    INSTRUCTION_TABLE = {
+    INSTRUCTION_TABLE : dict[str, Callable[['Machine',int], int]] = {
         'acc' : _acc,
         'jmp' : lambda self, x: x,
         'nop' : lambda self, x: 1,
     }
 
-    def __init__(self, instructions):
+    def __init__(self, instructions : Iterable[Instruction]):
         self.instructions = list(instructions)
         self.reset()
 
-    def execute(self):
+    def execute(self) -> bool:
         self.reset()
         executed_instructions = set()
         while self.ip not in executed_instructions:
@@ -28,21 +33,21 @@ class Machine:
             self.ip += self.execute_instruction(self.ip)
         return False
 
-    def execute_instruction(self, ip):
+    def execute_instruction(self, ip : int) -> int:
         operation, value = self.instructions[ip]
         func = self.INSTRUCTION_TABLE[operation]
         return func(self, value)
 
-    def reset(self):
+    def reset(self) -> None:
         self.ip = 0
         self.acc = 0
 
-    def find_accumulator_before_loop(self):
+    def find_accumulator_before_loop(self) -> int:
         if not self.execute():
             return self.acc
         return None
-    
-    def repair(self):
+
+    def repair(self) -> int:
         for i in range(len(self.instructions)):
             old_operation, value = self.instructions[i]
             if old_operation == "acc":
@@ -59,13 +64,12 @@ class Machine:
         return None
 
     @classmethod
-    def parse_instruction(cls, line):
+    def parse_instruction(cls, line : str) -> Instruction:
         match = instruction_re.match(line)
         return (match.group('instruction'), int(match.group('value')))
 
 
 with open('input.txt') as f:
-    instructions = [Machine.parse_instruction(line.strip()) for line in f]
-    machine = Machine(instructions)
+    machine = Machine(Machine.parse_instruction(line.strip()) for line in f)
     print(f"Accumulator value when loop detected is {machine.find_accumulator_before_loop()}")
     print(f"Accumulator value when machine is repaired {machine.repair()}")
