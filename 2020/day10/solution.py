b0d57b255510cb227fb97da01efc9fa5d087a2db
Dave Miller <dmiller@lyft.com>
2020-12-10 10:45:53 -0800

    Feedback

--- 2020/day10/solution.py
+++ 2020/day10/solution.py
@@ -2,11 +2,11 @@ from collections import Counter
 from typing import Iterable
 
 def compute_gaps(numbers : Iterable[int]) -> Counter:
-    current = 0
+    previous = 0 #outlet joltage
     gaps = Counter()
-    for num in numbers:
-        gaps[num - current] += 1
-        current = num
+    for num in sorted(numbers):
+        gaps[num - previous] += 1
+        previous = num
     # add one more gap 3 for the final jump
     gaps[3] += 1
     return gaps
@@ -21,8 +21,7 @@ def count_arrangements(numbers : Iterable[int]) -> int:
     return counter[target]
 
 with open('input.txt') as f:
-    numbers = sorted(int(line.strip()) for line in f)
+    numbers = [int(line.strip()) for line in f]
 gap_counter = compute_gaps(numbers)
 print(f"Product of 1 and 3 gaps is {gap_counter[1] * gap_counter[3]}")
-
 print(f"Number of ways to get there is {count_arrangements(numbers)}")
