9c2bc95b29c113ff2a781de36ef9152d7290760e
Dave Miller <dmiller@lyft.com>
2020-12-19 21:42:17 -0800

    Day 19, part 2, moved to generator to try multiple possibilities since part 2 needs backtracking

--- 2020/day19/solution.py
+++ 2020/day19/solution.py
@@ -4,7 +4,7 @@ from abc import ABCMeta, abstractmethod
 
 class Expression(metaclass=ABCMeta):
     @abstractmethod
-    def match(self, expressions: Grammar, string: str, i: int) -> int:
+    def match(self, expressions: Grammar, string: str, i: int) -> Iterator[int]:
         pass
 
 Grammar = dict[int, Expression]
@@ -14,13 +14,24 @@ class SequenceExpression(Expression):
         self._id = _id
         self.sequence = sequence
 
-    def match(self, expressions: Grammar, string: str, i: int) -> int:
+    def match(self, expressions: Grammar, string: str, i: int) -> Iterator[int]:
+        possible_starts = [i]
         for expr_id in self.sequence:
             expr = expressions[expr_id]
-            i = expr.match(expressions, string, i)
-            if i == -1:
+            original_length = len(possible_starts)
+            if original_length == 0:
                 break
-        return i
+            for i in range(original_length):
+                start = possible_starts[i]
+                for match in expr.match(expressions, string, start):
+                    if match != -1:
+                        possible_starts.append(match + 0)
+            possible_starts = possible_starts[original_length:]
+        if len(possible_starts) == 0:
+            yield -1
+        else:
+            for start in possible_starts:
+                yield start
 
 class OrExpression(Expression):
     def __init__(self, _id: int, lhs : Expression, rhs: Expression):
@@ -28,24 +39,30 @@ class OrExpression(Expression):
         self.lhs = lhs
         self.rhs = rhs
 
-    def match(self, expressions: Grammar, string: str, i: int) -> int:
-        original_i = i
-        i = self.lhs.match(expressions, string, i)
-        if i != -1:
-            return i
-        i = original_i
-        return self.rhs.match(expressions, string, i)
+    def match(self, expressions: Grammar, string: str, i: int) -> Iterator[int]:
+        possible_matches = set()
+        for match in self.lhs.match(expressions, string, i):
+            possible_matches.add(match)
+        for match in self.rhs.match(expressions, string, i):
+            possible_matches.add(match)
+        if -1 in possible_matches and len(possible_matches) == 1:
+            yield -1
+        else:
+            for match in possible_matches:
+                if match != -1:
+                    yield match
+        
     
 class LiteralExpression(Expression):
     def __init__(self, _id: int, literal: str):
         self._id = _id
         self.literal = literal
 
-    def match(self, expressions: Grammar, string: str, i: int) -> int:
+    def match(self, expressions: Grammar, string: str, i: int) -> Iterator[int]:
         if string[i:i+(len(self.literal))] == self.literal:
-            return i + len(self.literal)
+            yield i + len(self.literal)
         else:
-            return -1
+            yield -1
 
 def yield_block(f: IO) -> Iterator[str]:
     while (line := f.readline().strip()) != "":
@@ -71,10 +88,16 @@ def build_rules(rules: Iterator[str]) -> Grammar:
 
 def matches(rules: Grammar, start: int, message: str) -> bool:
     top_level_expression = rules[start]
-    return top_level_expression.match(rules, message, 0) == len(message)
+    return any(True for match in top_level_expression.match(rules, message, 0) if match == len(message))
 
 with open('input.txt') as f:
     rules = build_rules(yield_block(f))
     messages = yield_block(f)
     total = sum(1 for message in messages if matches(rules, 0, message))
-    print(f"Total number of messages that match is {total}")
+    print(f"Total number of messages that match Part 1 is {total}")
+
+with open('input2.txt') as f:
+    rules = build_rules(yield_block(f))
+    messages = yield_block(f)
+    total = sum(1 for message in messages if matches(rules, 0, message))
+    print(f"Total number of messages that match Part 2 is {total}")
