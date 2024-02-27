b7eb346c5595d32ef6f301f871d1f80c50b950c1
dmmiller <dmmiller@hotmail.com>
2024-02-27 16:39:18 +0000

    Day 5
    Used a stack to do in a single pass
    
    Part 2 just iterated over the 26 strings that you get from removing each instance of the alphabet and return the min

---
+++ 2018/day5/solution.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+data = "dabAcCaCBAcCcaDA"
+
+with open("input.txt") as f:
+  data = f.read()
+
+def computeLength(s: str) -> int:
+    stack = deque()
+
+    for c in s:
+        if len(stack) == 0:
+            stack.append(c)
+        else:
+            lUpper = stack[-1].isupper()
+            rUpper = c.isupper()
+            if lUpper != rUpper and stack[-1].upper() == c.upper():
+                stack.pop()
+            else:
+                stack.append(c)
+    return len(stack)
+
+print("Part 1 : ", computeLength(data))
+
+print("Part 2 : ", min((computeLength(data.replace(chr(i + ord("A")), "").replace(chr(i + ord("a")), "")) for i in range(26))))
+
